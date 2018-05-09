8b94f699d85dfdd3f1c1ee85b6f412b52e0635d2
TomHuang <huangjinj@live.com>
2018-05-09 22:24:23 +0800

    Modify the UI

---
+++ AppSettingsViewer/BindableBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppSettingsEditor
+{
+    public class BindableBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string name)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ AppSettingsViewer/SettingsItem.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppSettingsEditor
+{
+    public class SettingItem : BindableBase
+    {
+        string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (_name == value)
+                    return;
+                _name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        string _value;
+        public string Value
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                if (_value == value)
+                    return;
+                _value = value;
+                OnPropertyChanged("Value");
+            }
+        }
+
+        public string DataType
+        {
+            get
+            {
+                return Value?.GetType().Name;
+            }
+
+        }
+
+    }
+}
